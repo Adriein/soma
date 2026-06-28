3715f35877f6157557965ba70fce93bdae513e82
Adria Claret <adria.claret@gmail.com>
2026-06-28 10:55:45 +0200

    feat: adding fields into the users table

--- app/database/migrations/000001_init.up.sql
+++ app/database/migrations/000001_init.up.sql
@@ -11,6 +11,8 @@ CREATE TABLE IF NOT EXISTS so_users (
     sou_token VARCHAR,
     sou_token_secret VARCHAR,
     sou_token_verifier INT,
+    sou_telegram_chat_id INT NOT NULL,
+    sou_current_message_id INT NOT NULL,
     sou_date_add TIMESTAMP(0) WITHOUT TIME ZONE NOT NULL,
     sou_date_upd TIMESTAMP(0) WITHOUT TIME ZONE NOT NULL
 );
